2d6b7f68422c77d4e771e043d8e788703038448d
jsotterholt <jsotterholt@icloud.com>
2015-01-14 22:48:46 -0700

    initial file setup for .js
    OK NOW my changes should be reflected in the file.

--- hufford_josh_sdi-project2/hufford_josh_assignment2.js
+++ hufford_josh_sdi-project2/hufford_josh_assignment2.js
@@ -1,1 +1,15 @@
-alert("JavaScript works!");
+/*************************************************
+ * \author Josh Hufford
+ * \date 14-Jan-2014
+ * \file hufford_josh_assignment2.js
+ * \about Encapsulating procedures in JavaScript
+ */
+
+//Variables
+var retBool = false;
+var retNumber = 0;
+var retString = "";
+var boolStringified = "";
+//Function Declarations
+
+//Main Script
