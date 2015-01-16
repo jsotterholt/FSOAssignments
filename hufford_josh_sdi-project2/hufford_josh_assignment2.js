c2183e83a9cac0b9ac22cb697f28f32adbb6e407
jsotterholt <jsotterholt@icloud.com>
2015-01-15 20:06:39 -0700

    boolean and number functions
    added code for the boolean and number functions

--- hufford_josh_sdi-project2/hufford_josh_assignment2.js
+++ hufford_josh_sdi-project2/hufford_josh_assignment2.js
@@ -6,6 +6,8 @@
  */
 
 //Variables
+var confirmation = false;
+var numInParty = 0;
 var retBool = false;
 var retNumber = 0;
 var retString = "";
@@ -19,5 +21,35 @@ var checkpointProcedure = function(promptVar) {
     }
 }
 
+var waitForParty = function(wait, numPeople) {
+    var arrivals = 0;
+    var everyoneHere = false;
+    while(wait) {
+        if (arrivals < numPeople) {
+            console.log("Another person has arrived!");
+        } else {
+            console.log("Everyone has arrived!");
+            everyoneHere = true;
+            break;
+        }
+        arrivals++;
+    }
+    return everyoneHere;
+}
+var seatingOptions = function(people) {
+    var i = people;
+    var numWholeSplits = 0;
+    for (i; i > 0; i--) {
+        if (0 === people % i) {
+            numWholeSplits++;
+            console.log(people + " and " + i + " divide evenly.");
+        }
+    }
+}
+
 //Main Script
-checkpointProcedure(prompt("How many are in your party?"));
+numInParty = prompt("How many are in your party?");
+checkpointProcedure(numInParty);
+confirmation = confirm("You must wait for everyone in your party to arrive.");
+retBool = waitForParty(confirmation, numInParty);
+retNumber = seatingOptions(numInParty);
